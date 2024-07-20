81a9627c67c834aa833ebf4c37af2ae867cfcbd9
Aceds . <aceds.@Acedss-MacBook-Pro.local>
2024-07-21 01:27:14 +0700

    install some extentions

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 const port = 3000
 
+app.use(morgan('tiny'))
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send(`
+            <h1 style="color:red;">Home</h1>
+        `)
 })
 
 app.listen(port, () => {
